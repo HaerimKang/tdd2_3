2e0bb25862e86ffb42f05b25adbfa20d2f6745a0
hr_kang <hyerim513@gmail.com>
2023-07-06 08:42:04 +0900

    [refactoring] 03/04

--- TDD2_3/baseball.cpp
+++ TDD2_3/baseball.cpp
@@ -6,7 +6,14 @@ using namespace std;
 class Baseball
 {
 public:
-	void guess(const string& guessNumber)
+	bool isDuplicatedNumber(const string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
+	}
+
+	void assertIllegalArgument(const string& guessNumber)
 	{
 		if (guessNumber.length() != 3)
 		{
@@ -15,16 +22,18 @@ public:
 
 		for(char ch : guessNumber)
 		{
-			if (ch < '0' || ch > '9')
-			{
-				throw length_error("Must be number.");
-			}
+			if (ch >= '0' && ch <= '9')
+				continue;
+			throw length_error("Must be number.");
 		}
-		if(guessNumber[0] == guessNumber[1] 
-			|| guessNumber[0] == guessNumber[2] 
-			|| guessNumber[1] == guessNumber[2])
+		if(isDuplicatedNumber(guessNumber))
 		{
 			throw length_error("Must not have the same number.");
 		}
+	}
+
+	void guess(const string& guessNumber)
+	{
+		assertIllegalArgument(guessNumber);
 	}
 };
