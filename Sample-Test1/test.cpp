f4dd61ddae7059e34e87d91855881fd268dc3375
hr_kang <hyerim513@gmail.com>
2023-07-05 17:01:19 +0900

    [feature] 01 ThrowExceptionWhenInputLengthIsUnmached

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
+#include "../TDD2_3/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <string>
+using namespace std;
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
